1e027f640646973db54508e55f9b6d286e3bac12
Andrei Terziev <andrei.terziev@egt-bg.com>
2021-04-06 12:10:52 +0300

    Add coints and buttons

---
+++ app/src/main/java/com/andy/gamesmultigame/CardsView.java
@@ -0,0 +1,10 @@
+package com.andy.gamesmultigame;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class CardsView extends SurfaceView {
+    public CardsView(Context context) {
+        super(context);
+    }
+}
--- app/src/main/java/com/andy/gamesmultigame/MainActivity.java
+++ app/src/main/java/com/andy/gamesmultigame/MainActivity.java
@@ -2,13 +2,40 @@ package com.andy.gamesmultigame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
+    TextView txtCredit, txtBet;
+    int iCredit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        iCredit = 1000;
+
+        txtBet = (TextView)findViewById(R.id.txtbet);
+        txtBet.setTextColor(Color.WHITE);
+        txtBet.setTextSize(30);
+
+        txtCredit = (TextView)findViewById(R.id.txtcredit);
+        txtCredit.setTextColor(Color.WHITE);
+        txtCredit.setTextSize(30);
+        txtCredit.setText(String.valueOf(iCredit));
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
